51850ce3d3a27bcb1919cf4c62b44f66774f27ab
Fernanda <fernandalmrj@hotmail.com>
2022-04-06 23:31:14 -0300

    primeiro commit

---
+++ 5- HTML/index1.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+
+<html> 
+  <head>
+    <meta charset="uft-8">
+    <title>fernanda mota</title>
+  </head>
+  <body>
+	<header>
+	<img> src="eu linda.jpg">
+      <h1>fernanda mota</h1>
+	</header>
+	<section>
+	  <header>
+	    <h2>Posts</h2>
+	   <header>
+	   <articles>
+	     <header>
+	       <h3>Post #1</h3>
+		 </header>
+	   </article>
+	  <section>
+	  <footer></footer>
+    </body>
+  </html>
